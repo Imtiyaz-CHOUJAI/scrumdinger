6d495fd3b937a49a3acc270ebcb196b9e0f1e046
Imtiyaz CHOUJAI <33980447+Imtiyaz-CHOUJAI@users.noreply.github.com>
2023-02-25 21:16:39 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Imtiyaz Choujai on 25.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
